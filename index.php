fecfb59cb3c52d1fc4d527b6294a924ed1a2020d
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-01-27 21:28:00 +0800

    Add css sht

--- index.php
+++ index.php
@@ -4,32 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        <?php include "styles.css" ?>
+    </style>
 </head>
 <body>
-    <form action="index.php " method="POST">
-        <label for="name">Name: </label>
-        <input type="text" name="name" id="name">
-        <label for="pwd">Password: </label>
-        <input type="password" name="pwd" id="pwd">
-        <input type="submit" value="Save to DB">
-    </form>
-    <?php 
+    <main>
+        <div class="signInPrompt">
+            <form action="index.php " method="POST">
 
-    include "DB.php";
-    $formCollect = new FormCollection();
-    if (isset($_POST["name"]) && isset($_POST["pwd"])){
-        $name = $_POST["name"];
-        $pwd = $_POST["pwd"];
-    
-        $formCollect->actionQuery("INSERT INTO Credentials(NAME, PASSWORD ) VALUES('$name', '$pwd')");
-    }
-    $res = $formCollect->selectQuery("SELECT * FROM Credentials;");
-    for ($i = 0; $i < count($res); $i++){
-        for ($q = 0; $q < count($res[$i]); $q++){
-            echo  $res[$i][$q] . "   ";
-        }
-        echo "<br>";
-    }
-    ?>
+                <label for="name">Name: </label>
+                <input type="text" name="name" id="name">
+                <label for="pwd">Password: </label>
+                <input type="password" name="pwd" id="pwd">
+                <input type="submit" value="Save to DB">
+            </form>
+            <?php 
+
+            include "DB.php";
+            $formCollect = new FormCollection();
+            if (isset($_POST["name"]) && isset($_POST["pwd"])){
+                $name = $_POST["name"];
+                $pwd = $_POST["pwd"];
+            
+                $formCollect->actionQuery("INSERT INTO Credentials(NAME, PASSWORD ) VALUES('$name', '$pwd')");
+            }
+            $res = $formCollect->selectQuery("SELECT * FROM Credentials;");
+            for ($i = 0; $i < count($res); $i++){
+                for ($q = 0; $q < count($res[$i]); $q++){
+                    echo  $res[$i][$q] . "   ";
+                }
+                echo "<br>";
+            }
+            ?>
+        
+        </div>
+    </main>
 </body>
 </html>
